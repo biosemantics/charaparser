b09dc567eb9f8c3e097b5f445cd43f0e1995df9f
rodenhausen <thomas.rodenhausen@gmail.com>
2013-06-13 10:04:29 -0700

    fixed tokenization of sentence, to seperate punctuation immediately following a word

--- src/semanticMarkup/ling/transform/lib/WhitespaceTokenizer.java
+++ src/semanticMarkup/ling/transform/lib/WhitespaceTokenizer.java
@@ -2,6 +2,8 @@ package semanticMarkup.ling.transform.lib;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import semanticMarkup.ling.Token;
 import semanticMarkup.ling.transform.ITokenizer;
@@ -12,11 +14,22 @@ import semanticMarkup.ling.transform.ITokenizer;
  */
 public class WhitespaceTokenizer implements ITokenizer {
 
+	private Pattern wordPunctuationPattern = Pattern.compile("(.+)(\\W+)");
+	
 	@Override
 	public List<Token> tokenize(String text) {
 		List<Token> tokens = new ArrayList<Token>();
 		for(String token : text.split("\\s+")) {
-			tokens.add(new Token(token));
+			
+			Matcher matcher = wordPunctuationPattern.matcher(token);
+			if(matcher.matches()) {
+				String word = matcher.group(1);
+			 	String punctuation = matcher.group(2);
+			 	tokens.add(new Token(word));
+			 	tokens.add(new Token(punctuation));
+			} else {
+				tokens.add(new Token(token));
+			}			
 		}
 		return tokens;
 	}
