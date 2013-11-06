a1d6a9a6ea402d62065b47b07d3a330d1529d8e4
rodenhausen <thomas.rodenhausen@gmail.com>
2013-11-06 12:09:29 -0700

    modified oto_secret db column

--- src/semanticMarkup/run/IPlantLearnRun.java
+++ src/semanticMarkup/run/IPlantLearnRun.java
@@ -66,7 +66,7 @@ public class IPlantLearnRun extends AbstractRun {
 				databaseUser, databasePassword);
 		
 		String sql = "CREATE TABLE IF NOT EXISTS datasetprefixes (prefix varchar(100) NOT NULL, glossary_version varchar(10), oto_uploadid int(11) NOT NULL DEFAULT '-1', " +
-				"oto_secret varchar(100), created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (prefix)) CHARACTER SET utf8 engine=innodb";
+				"oto_secret varchar(100) NOT NULL DEFAULT '', created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (prefix)) CHARACTER SET utf8 engine=innodb";
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
 		preparedStatement.execute();
 		
