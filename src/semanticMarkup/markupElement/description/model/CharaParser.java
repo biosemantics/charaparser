053768d2791a6f2515802772f05d25dc760d93d6
Thomas Rodenhausen <thomas.rodenhausen@gmail.com>
2013-11-16 21:41:57 -0700

    adapted bindings and model to new schema

---
+++ src/semanticMarkup/markupElement/description/model/CharaParser.java
@@ -0,0 +1,34 @@
+package semanticMarkup.markupElement.description.model;
+
+public class CharaParser {
+	private String charaparserVersion;
+	private String charaparserUser;
+	private String glossaryType;
+	private String glossaryVersion;
+	
+	public String getCharaparserVersion() {
+		return charaparserVersion;
+	}
+	public void setCharaparserVersion(String charaparserVersion) {
+		this.charaparserVersion = charaparserVersion;
+	}
+	public String getGlossaryType() {
+		return glossaryType;
+	}
+	public void setGlossaryType(String glossaryType) {
+		this.glossaryType = glossaryType;
+	}
+	public String getGlossaryVersion() {
+		return glossaryVersion;
+	}
+	public void setGlossaryVersion(String glossaryVersion) {
+		this.glossaryVersion = glossaryVersion;
+	}
+	public String getCharaparserUser() {
+		return charaparserUser;
+	}
+	public void setCharaparserUser(String charaparserUser) {
+		this.charaparserUser = charaparserUser;
+	}
+	
+}
