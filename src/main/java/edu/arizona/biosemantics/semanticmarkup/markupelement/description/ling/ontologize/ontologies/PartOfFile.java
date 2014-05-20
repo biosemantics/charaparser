80f62dec51d44d894605b1f3f7e4ff3dcc8375fe
hongcui <hong1.cui@gmail.com>
2014-05-20 16:47:49 -0700

    adding files

---
+++ src/main/java/edu/arizona/biosemantics/semanticmarkup/markupelement/description/ling/ontologize/ontologies/PartOfFile.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package edu.arizona.biosemantics.semanticmarkup.markupelement.description.ling.ontologize.ontologies;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import edu.arizona.biosemantics.semanticmarkup.log.LogLevel;
+import edu.arizona.biosemantics.semanticmarkup.markupelement.description.ling.ontologize.IOntology;
+
+/**
+ * @author Hong Cui
+ *
+ */
+
+public class PartOfFile implements IOntology {
+
+	private static Hashtable<String, ArrayList<String>> partof = null;
+	private File file;
+	
+	@Inject
+	public PartOfFile(@Named("Run_OutDirectory") String runOutDirectory){
+		try{
+		file = new File("workspace/fungi/", "ontopartof.bin");
+		//read in serialized partof hashtable
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+				file));
+		// Deserialize the object
+		if(partof !=null)
+			partof = (Hashtable<String, ArrayList<String>>) in.readObject();  
+		}catch(Exception e){
+			e.printStackTrace();
+			log(LogLevel.ERROR, e.toString());
+		}
+
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.arizona.biosemantics.semanticmarkup.markupelement.description.ling.ontologize.IOntology#isPart(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public boolean isPart(String part, String parent) {
+		
+		return partof.get(part)==null? false : partof.get(part).contains(parent);
+	}
+
+}
---
+++ src/main/java/edu/arizona/biosemantics/semanticmarkup/markupelement/description/ling/ontologize/ontologies/PartOfSerialization.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package edu.arizona.biosemantics.semanticmarkup.markupelement.description.ling.ontologize.ontologies;
+
+/**
+ * 
+ */
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+
+/**
+ * @author Hong Cui
+ * 
+ * turn a csv file (part, parent) to a serialized Hashtable<String, ArrayList<String>>
+ * 
+ *
+ */
+public class PartOfSerialization {
+	Hashtable<String, ArrayList<String>> parts = new Hashtable<String, ArrayList<String>>();
+	/**
+	 * 
+	 */
+	public PartOfSerialization(String input, String output) {
+		try{
+
+			FileInputStream fstream = new FileInputStream(input);
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String line;
+			while ((line = br.readLine()) != null)   {
+				line = line.trim();
+				if(line.length()==0) continue;
+				String[] partof = line.split("\\s*,\\s*");
+				ArrayList<String> parents = parts.get(partof[0]);
+				if(parents == null){
+					parents = new ArrayList<String>();
+				}
+				parents.add(partof[1]);
+				parts.put(partof[0], parents);
+			}
+			br.close();
+			//serialization
+			File file = new File(output);
+			ObjectOutput out = new ObjectOutputStream(
+					new FileOutputStream(file));
+			out.writeObject(parts);
+			out.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//String input = "Lactarius_part_of_preferredtermsonly.txt";
+		String input = "C:/Users/updates/CharaParserTest/Ontologies/mushroom_partof.txt";
+		String output = "ontopartof.bin";
+		String folder = "workspace/fungi/";
+		PartOfSerialization pos = new PartOfSerialization(input, folder+output);
+
+	}
+
+}
