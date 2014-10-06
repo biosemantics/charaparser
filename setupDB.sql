c2a7094d0e458e8edbb2fb35a231db4ce47ba1db
rodenhausen <thomas.rodenhausen@gmail.com>
2014-10-06 15:55:38 -0700

    add setupDB

---
+++ setupDB.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `datasetprefixes` (
+	prefix varchar(100) NOT NULL, 
+	glossary_version varchar(10), 
+	oto_uploadid int(11) NOT NULL DEFAULT '-1', 
+	oto_secret varchar(100) NOT NULL DEFAULT '',
+	created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (prefix)
+) CHARACTER SET utf8 engine=innodb";
