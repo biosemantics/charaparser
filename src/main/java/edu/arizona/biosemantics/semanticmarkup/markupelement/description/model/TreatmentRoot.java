47449d37daba60f9579f459e4b42ee32d694fe1e
rodenhausen <thomas.rodenhausen@gmail.com>
2014-10-31 03:46:07 -0700

    add key parsing

--- src/main/java/edu/arizona/biosemantics/semanticmarkup/markupelement/description/model/TreatmentRoot.java
+++ src/main/java/edu/arizona/biosemantics/semanticmarkup/markupelement/description/model/TreatmentRoot.java
@@ -5,14 +5,33 @@ import java.util.List;
 
 public class TreatmentRoot {
 
-	private List<Description> descriptions = new LinkedList<Description>();
-
+	private List<Description> descriptionDescriptions = new LinkedList<Description>();
+	private List<Description> keyStatementDescriptions = new LinkedList<Description>();
+	
 	public List<Description> getDescriptions() {
-		return descriptions;
+		List<Description> allDescriptions = new LinkedList<Description>();
+		allDescriptions.addAll(descriptionDescriptions);
+		allDescriptions.addAll(keyStatementDescriptions);
+		return allDescriptions;
 	}
 
-	public void setDescriptions(List<Description> descriptions) {
-		this.descriptions = descriptions;
+	public List<Description> getDescriptionDescriptions() {
+		return descriptionDescriptions;
 	}
+
+	public void setDescriptionDescriptions(List<Description> descriptionDescriptions) {
+		this.descriptionDescriptions = descriptionDescriptions;
+	}
+
+	public List<Description> getKeyStatementDescriptions() {
+		return keyStatementDescriptions;
+	}
+
+	public void setKeyStatementDescriptions(
+			List<Description> keyStatementDescriptions) {
+		this.keyStatementDescriptions = keyStatementDescriptions;
+	}
+
+
 
 }
