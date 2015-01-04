9e0e233b5dc1656dbf36cf75d75bb25769fae5fd
hongcui <hong1.cui@gmail.com>
2015-01-04 15:36:07 -0700

    prep for remote

--- src/main/java/edu/arizona/biosemantics/semanticmarkup/markupelement/description/transform/MarkupDescriptionTreatmentTransformer.java
+++ src/main/java/edu/arizona/biosemantics/semanticmarkup/markupelement/description/transform/MarkupDescriptionTreatmentTransformer.java
@@ -319,7 +319,7 @@ public class MarkupDescriptionTreatmentTransformer extends AbstractDescriptionTr
 	 * @param glossaryDownload
 	 * @param download
 	 */
-	protected void initGlossary(GlossaryDownload glossaryDownload, Download download) {
+	/*protected void initGlossary(GlossaryDownload glossaryDownload, Download download) {
 		//glossary
 		//add the syn set of the glossary
 		HashSet<Term> gsyns = new HashSet<Term>();
@@ -395,7 +395,7 @@ public class MarkupDescriptionTreatmentTransformer extends AbstractDescriptionTr
 			log(LogLevel.ERROR, "Exception", e);
 		}
 	}
-	
+	*/
 	
 	/**
 	 * notes: OTO Webservice should probably only return one term category list.
@@ -407,7 +407,7 @@ public class MarkupDescriptionTreatmentTransformer extends AbstractDescriptionTr
 	 * For structure terms, both singular and plural forms are included in the synonyms
 	 * @param otoGlossary
 	 */
-	/*protected void initGlossary(GlossaryDownload glossaryDownload, Download download) {
+	protected void initGlossary(GlossaryDownload glossaryDownload, Download download) {
 
 		//add the syn set of the glossary
 		HashSet<Term> gsyns = new HashSet<Term>();
@@ -501,7 +501,7 @@ public class MarkupDescriptionTreatmentTransformer extends AbstractDescriptionTr
 			}
 		}
 	
-	}*/
+	}
 	
 	private void storeInLocalDB(GlossaryDownload glossaryDownload, Download download, 
 			String tablePrefix) {
