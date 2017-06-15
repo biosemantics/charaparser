c02cb9f3d5ddfee8b84b6bfe377fd69db0e0a7a4
danveno <danveno@163.com>
2017-06-15 12:45:26 -0700

    modify the glossary version column

--- setupDB.sql
+++ setupDB.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `datasetprefixes` (
 	prefix varchar(100) NOT NULL, 
-	glossary_version varchar(10), 
+	glossary_version varchar(100), 
 	oto_uploadid int(11) NOT NULL DEFAULT '-1', 
 	oto_secret varchar(100) NOT NULL DEFAULT '',
 	created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
